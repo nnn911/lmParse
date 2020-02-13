9bc99b2bac32585ed245ed121c7b139f590aba9c
nnn911 <60605180+nnn911@users.noreply.github.com>
2020-02-13 15:55:32 -0500

    Initial commit

---
+++ lmParse.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+
+class Log:
+    def __init__(self, fname=None):
+        self.data = pd.DataFrame()
+        self.N = []
+        self.run = 0
+        if fname:
+            self.parseLog(fname)
+
+    def __getitem__(self, arg):
+        if arg.lower() == 'index':
+            return self.data.index
+        elif arg.lower() == 'n':
+            return self.getN()
+        else:
+            try:
+                return self.data[arg]
+            except:
+                raise KeyError('Key {} not found! Valid keys are:'.format(arg),
+                               ' '.join(self.keys()))
+
+    def getN(self):
+        N = np.array(self.N)
+        if np.all(N == self.N[0]):
+            return self.N[0]
+        else:
+            return N
+
+    def keys(self):
+        return self.data.keys()
+
+    def parseLog(self, fname):
+        newData = []
+        headerline = False
+        dataline = False
+        tailline = False
+        with open(fname, 'r') as f:
+            for line in f:
+                if (line[:12] == 'Memory usage') or \
+                   (line[:12] == 'Per MPI rank'):
+                    headerline = True
+                    tailline = False
+                    continue
+                if line[:4] == 'Loop':
+                    headerline = False
+                    dataline = False
+                    tailline = True
+                    self.run += 1
+                    self.N.append(int(line.split()[-2]))
+                if headerline:
+                    headerline = False
+                    dataline = True
+                    categories = line.strip().split()
+                    continue
+                if (dataline) and not (headerline) and not (tailline):
+                    l = [float(v) for v in line.strip().split()]
+                    newData.append({c: v for c, v in zip(categories, l)})
+                    newData[-1]['Run'] = self.run
+        self.data = self.data.append(newData, ignore_index=True)
