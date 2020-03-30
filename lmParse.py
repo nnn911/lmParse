431dbd7b4bd51e2043f1d5877b8e26352f16c347
Daniel Utt <nnn9111@gmail.com>
2020-03-30 10:38:34 +0200

    Add obj to store run specific info like wall time

--- lmParse.py
+++ lmParse.py
@@ -1,32 +1,46 @@
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 
 class Log:
     def __init__(self, fname=None):
         self.data = pd.DataFrame()
-        self.N = []
+        self.runInfo = defaultdict(dict)
         self.logFiles = []
         self.run = 0
         if fname:
             self.parseLog(fname)
 
     def __getitem__(self, arg):
-        if arg.lower() == 'index':
-            return self.data.index
-        elif arg.lower() == 'n':
-            return self.getN()
+        if isinstance(arg, str):
+            if arg.lower() == 'index':
+                return self.data.index
+            elif arg.lower() == 'n':
+                return self.getN()
+            else:
+                try:
+                    return self.data[arg]
+                except KeyError:
+                    err = 'Key {} not found! Valid keys are: {}'.format(
+                        arg, ' '.join(self.keys()))
+                    raise KeyError(err)
+        elif isinstance(arg, int):
+            if arg in self.runInfo:
+                return self.runInfo[arg]
+            elif arg == -1:
+                return self.runInfo[self.run-1]
+            else:
+                raise KeyError(
+                    'Key {} not found! Valid numeric keys are:'.format(arg),
+                    ' '.join([str(k) for k in self.runInfo.keys()]))
         else:
-            try:
-                return self.data[arg]
-            except KeyError:
-                raise KeyError('Key {} not found! Valid keys are:'.format(arg),
-                               ' '.join(self.keys()))
+            raise KeyError('Key {} not found!'.format(arg))
 
     def getN(self):
-        N = np.array(self.N)
-        if np.all(N == self.N[0]):
-            return self.N[0]
+        N = np.array([self.runInfo[i]['N'] for i in range(self.run)])
+        if np.all(N == N[0]):
+            return N[0]
         else:
             return N
 
@@ -46,7 +60,7 @@ class Log:
         with open(fname, 'r') as f:
             for line in f:
                 if (line[:12] == 'Memory usage') or \
-                   (line[:12] == 'Per MPI rank'):
+                        (line[:12] == 'Per MPI rank'):
                     headerline = True
                     tailline = False
                     continue
@@ -54,8 +68,9 @@ class Log:
                     headerline = False
                     dataline = False
                     tailline = True
+                    self.runInfo[self.run]['N'] = int(line.split()[-2])
+                    self.runInfo[self.run]['LoopTime'] = float(line.split()[3])
                     self.run += 1
-                    self.N.append(int(line.split()[-2]))
                 if headerline:
                     headerline = False
                     dataline = True
@@ -65,5 +80,8 @@ class Log:
                     lines = [float(v) for v in line.strip().split()]
                     newData.append({c: v for c, v in zip(categories, lines)})
                     newData[-1]['Run'] = self.run
+                if 'Stopping criterion' in line:
+                    sc = line.split('=')[-1].strip()
+                    self.runInfo[self.run-1]['StoppingCriterion'] = sc
         self.data = self.data.append(newData, ignore_index=True)
         self.logFiles.append(fname)
